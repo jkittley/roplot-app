a0c74bfbc9768f7b3e4cf3cc0cdc31edd860e013
Jacob Kittley-Davies <jacob@kittley.com>
2018-04-26 19:58:37 +0100

    Print manager and connections

--- www/js/index.js
+++ www/js/index.js
@@ -19,15 +19,27 @@
 
 var app = {
 
-    printerConnected: true,
+    printerManager: null,
 
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
         snap.addEventListener('click', this.takePicture.bind(this), false);
         album.addEventListener('click', this.openPicture.bind(this), false);
+
+        buttonRefreshPrinterList.addEventListener('click', this.printersScanStart.bind(this), false);
+        printerControlDisconnect.addEventListener('click', this.disconnectPrinter.bind(this), false);
+
+        printerRequestConfig.addEventListener('click', this.printerNowConnected.bind(this), false);
+
         $('button[data-page]').click(this.go);
         $('a[data-page]').click(this.go);
+
+        app.printerManager = new PrintManager();
+        app.printerManager.addListener('printerConfig', function(eventData) {
+            $('#printerConfig').html(JSON.stringify(eventData, undefined, 2));
+        }.bind(this));
+
     },
 
     // deviceready Event Handler
@@ -37,9 +49,12 @@ var app = {
         this.checkPrinterConnection();
     },
 
+    quickAlert: function(title, msg, btnName="Done", cb=null) {
+        navigator.notification.alert(msg, cb, title, btnName);
+    },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        
         console.log('Received Event: ' + id);
     },
 
@@ -71,17 +86,103 @@ var app = {
         });
     },
 
+    pagesActions: {
+        "printer": function() {
+            if (app.printerManager.printerConnected()) {
+                printerPageManage.hidden = false;
+                printerPageScan.hidden = true;
+            } else {
+                printerPageManage.hidden = true;
+                printerPageScan.hidden = false;
+                app.printersScanStart();
+            }
+        }
+    },
+
     go: function(e) {   
         app.checkPrinterConnection();
-        var target = $(e.target); 
-        if (!target.attr('data-page')) target = target.closest('[data-page]');
-        var pgname = '#page-' + target.data('page');
+        var pgname = null;
+        if (typeof e === 'string') {
+            pgname = e;
+        } else {
+            var target = $(e.target); 
+            if (!target.attr('data-page')) target = target.closest('[data-page]');
+            pgname = target.data('page');
+        }
         $('.page').addClass('d-none');
-        $(pgname).removeClass('d-none');
+        $('#page-' + pgname).removeClass('d-none');
+        // Execute custom actions
+        if (app.pagesActions.hasOwnProperty(pgname)) app.pagesActions[pgname]();
+    },
+
+
+    printersScanStart: function() {
+        buttonRefreshPrinterList.hidden = true;
+        spinnerRefreshPrinterList.hidden = false;
+        $('#printerList').empty();
+
+        var onComplete = function() {
+            buttonRefreshPrinterList.hidden = false;
+            spinnerRefreshPrinterList.hidden = true;
+        };
+
+        app.printerManager.scan(
+            function(printer) {
+                var badge = $('<span>').addClass("badge badge-primary badge-pill p-2").html(printer.id);
+                $('<li>').addClass('list-group-item')
+                    .addClass('list-option-printer')
+                    .addClass('d-flex')
+                    .addClass('justify-content-between')
+                    .addClass('align-items-center')
+                    .html(printer.name)
+                    .append(badge)
+                    .on('click', function() { 
+                        buttonRefreshPrinterList.hidden = true;
+                        spinnerRefreshPrinterList.hidden = true;
+                        $('.list-option-printer').unbind( "click" );
+                        $(this).html('<i class="fas fa-circle-notch fa-spin"></i> Connecting...').addClass('active').removeClass('justify-content-between').removeClass('d-flex');
+                        app.printerManager.connectTo(printer, app.printerNowConnected, app.printerConnectionFailed); 
+                    })
+                    .appendTo('#printerList');
+            }, 
+            function(msg) {
+                app.quickAlert("Error", msg);
+                onComplete();
+            },
+            onComplete);
+    },
+
+    printerNowConnected: function() {
+        // navigator.notification.alert("Printer connected");
+        $('#printerConfig').html('Loading...');
+        app.printerManager.getPrinter().requestConfig(function(success, info) {
+            if (success) $('#printerConfig').html('Fetching...');
+            else $('#printerConfig').html('Error: '+info);
+        });
+        app.go("printer");
+    },
+
+    printerConnectionFailed: function() {
+        app.quickAlert("Error", "Connection to printer failed to selected printer");
+        app.go("printer");
+    },
+
+    disconnectPrinter: function() {
+        app.printerManager.disconnectPrinter(function(success) {
+            if (success) {
+                
+                app.quickAlert("Disconnected", "The printer has been successfully disconnected");
+                $('#printerConfig').html('Loading...');
+                app.go("printer");
+            } else {
+                $('#printerConfig').html('An error occurred');
+                app.quickAlert("Error", "Disconnection failed, please try again");
+            }
+        });
     },
 
     checkPrinterConnection: function() {
-        if (app.printerConnected) {
+        if (app.printerManager.printerConnected()) {
             $('.needs-connection').prop('disabled', false);
             $('.no-connection-msg').addClass('d-none');
             $('#btn-connect span').html(" Manage");
---
+++ www/js/print-manager.js
@@ -0,0 +1,250 @@
+class PrintManager {
+    
+    constructor() {
+        this._printer = null;
+        this._bleAvailable = false;
+        this._scanTimeout = null;
+        this.listeners = new Map();
+    }
+
+    addListener(label, callback) {
+        this.listeners.has(label) || this.listeners.set(label, []);
+        this.listeners.get(label).push(callback);
+    }
+
+    isFunction(obj) {  
+        return typeof obj == 'function' || false;
+    }
+    
+    removeListener(label, callback) {  
+        let listeners = this.listeners.get(label), index;
+    
+        if (listeners && listeners.length) {
+            index = listeners.reduce((i, listener, index) => {
+                return (this.isFunction(listener) && listener === callback) ?
+                    i = index :
+                    i;
+            }, -1);
+    
+            if (index > -1) {
+                listeners.splice(index, 1);
+                this.listeners.set(label, listeners);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    emitEvent(label, json) {  
+        let listeners = this.listeners.get(label);
+        if (listeners && listeners.length) {
+            listeners.forEach((listener) => {
+                listener(json); 
+            });
+            return true;
+        }
+        return false;
+    }
+
+
+    _processUpdates(updJson) {
+        this.emitEvent(updJson.type, updJson);
+    }
+    
+    isRadioEnabled(cb=null) {
+        ble.isEnabled(
+        function() {
+            this._bleAvailable = true;
+            if (cb!==null) cb(true);
+        }.bind(this), 
+        function() {
+            this._bleAvailable = false;
+            if (cb!==null) cb(false);
+        }.bind(this));
+    }
+
+    printerConnected() {
+        return this._printer !== null;
+    }
+
+    getPrinter() {
+        if (!this.printerConnected()) return null;
+        return this._printer;
+    }
+
+    scan(cbEachDeviceFound=null, cbError=null, cdComplete=null, scanFor=5000) {
+        this.isRadioEnabled(function(available) {
+            if (available) {
+                this.startScan(cbEachDeviceFound, cbError, cdComplete, scanFor);
+            } else {
+                cbError("Bluetooth is not available. Without it no remote printers can be scanned for. Please make sure it is enabled in you device's settings.");
+            }
+        }.bind(this))
+    }
+
+    startScan(cbEachDeviceFound, cbError, cdComplete, scanFor) {
+        ble.startScan([], function(device) { 
+            console.log(JSON.stringify(device));
+            var p = new Printer(device.id, device.name, device.advertising, device.rssi, this)
+            if (cbEachDeviceFound !== null) cbEachDeviceFound(p);
+        }.bind(this), function() { 
+            if (cbError !== null) cbError("Scan failed to start");
+            if (cdComplete !== null) cdComplete();
+        }.bind(this));
+        this._scanTimeout = setTimeout(ble.stopScan,
+            scanFor,
+            function() {
+                if (cdComplete !== null) cdComplete();
+            },
+            function() { 
+                if (cbError !== null) cbError("Failed to terminate scan");
+            }
+        );
+    }
+
+    connectTo(printer, cbConnect=null, cbDisconnect=null) {
+        if (this._scanTimeout !== null) clearTimeout(this._scanTimeout);
+        ble.stopScan();
+        printer.connect(
+            function() { 
+                // Connection established
+                this._printer = printer;
+                if (cbConnect !== null) cbConnect();
+            }.bind(this),
+            function() { 
+                // Connection fails to establish or subsequently fails
+                this._printer = null;
+                if (cbDisconnect !== null) cbDisconnect();
+            }.bind(this),
+        );
+    }
+
+    disconnectPrinter(cb=null) {
+        this._printer.disconnect(function() {
+            this._printer = null;
+            if (cb !== null) cb(true);
+        }.bind(this));
+    }
+}
+
+// ------
+
+var bluefruit = {
+    serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+    txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
+    rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
+};
+
+class Printer {
+
+    constructor(id, name, advertising, rssi, manager) {
+        this.id = id;
+        this.name = (name === undefined || name === null) ? "No name" : name;
+        this.rssi = rssi;
+        this.isConnected = false;
+        this.data = "";
+        this.writeMethod = ble.write;
+        this.printManager = manager;
+    }
+
+    _determineWriteType(peripheral) {
+        var characteristic = peripheral.characteristics.filter(function(element) {
+            if (element.characteristic.toLowerCase() === bluefruit.txCharacteristic) {
+                return element;
+            }
+        })[0];
+        this.writeMethod = (characteristic.properties.indexOf('WriteWithoutResponse') > -1) ? ble.writeWithoutResponse : ble.write;
+    }
+
+    connect(cbConnect=null, cbDisconnect=null) {
+        this.isConnected = false;
+        var onConnect = function(peripheral) {
+            try {
+                this._determineWriteType(peripheral);
+                ble.startNotification(this.id, bluefruit.serviceUUID, bluefruit.rxCharacteristic, this._onData.bind(this), this._onConnectionError.bind(this));
+                this.isConnected = true;
+                if (cbConnect!==null) cbConnect();
+            } catch (error) {
+                console.log(error);
+                this._onConnectionError("Failed to connect.");
+                if (cbDisconnect!==null) cbDisconnect();
+            } 
+        }.bind(this);
+        var onDisconnect = function() {
+            this._onConnectionError("Lost connection to device.");
+            if (cbDisconnect!==null) cbDisconnect();
+        }.bind(this)
+
+        ble.connect(this.id, onConnect, onDisconnect);
+    }
+
+    _onData(data) {
+        // Append data and remove all newlines, tabs, etc.
+        this.data += this.bytesToString(data).replace(/(\r\n\t|\n|\r\t)/gm,"");
+        var num_open_braces = (this.data.match(/{/g) || []).length;
+        var num_close_braces = (this.data.match(/}/g) || []).length;
+        // If the string is a complete json sting i.e. num of { == num of }
+        if (num_open_braces === num_close_braces) {
+            try {
+                console.log(this.data);
+                var json = JSON.parse(this.data);
+                this.printManager._processUpdates(json);
+            } catch(e) {
+                console.log("Parse error", e);
+            } finally {
+                this.data = "";
+            }
+        } else {
+            console.log("incomplete packet");
+        }
+    }
+
+    _onConnectionError(msg) {
+        this.isConnected = false;
+    }
+
+    disconnect(cb=null) {
+        ble.disconnect(
+            this.id, 
+            function() {
+                this.isConnected = false;
+                if (cb!==null) cb(true);
+            }.bind(this), 
+            function() {
+                if (cb!==null) cb(false);
+            }.bind(this));
+    }
+
+    // Request the printer configuration
+    requestConfig(cb=null) {
+        this.sendCommand("getconfig");
+    }
+
+    sendCommand(command, cb=null) {
+        var cmd = JSON.stringify({"type": command});
+        console.log("sending command: ", cmd, " to: ", this.id);
+        ble.write(
+            this.id,
+            bluefruit.serviceUUID,
+            bluefruit.txCharacteristic,
+            this.stringToBytes(cmd),
+            function(e) { console.log("ACK"); if (cb!==null) cb(true, e); },
+            function(e) { console.log("No ACK"); if (cb!==null) cb(false, e); }          
+        );
+    }
+
+    // ASCII only
+    bytesToString(buffer) {
+        return String.fromCharCode.apply(null, new Uint8Array(buffer));
+    }
+
+    // ASCII only
+    stringToBytes(string) {
+        var array = new Uint8Array(string.length);
+        for (var i = 0, l = string.length; i < l; i++) {
+            array[i] = string.charCodeAt(i);
+        }
+        return array.buffer;
+    }
+
+}
